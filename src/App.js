314c358cca03c1529a7c07abfeaedc96aa707730
Tebogo Nomnqa <tebzax2@gmail.com>
2022-01-17 15:26:06 +0200

    style: use globalstyles from styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-require('./App.css');
-
 function App() {
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import App from './App';
+import GlobalStyles from './styles/Global';
 
 ReactDOM.render(
   <BrowserRouter>
+    <GlobalStyles />
     <Routes>
       <Route index element={<App />} />
       <Route path="/search" element={<App />} />
