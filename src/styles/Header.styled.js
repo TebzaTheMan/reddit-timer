1a9d6ab296f86cd862fd5528625e0d899b8be746
Tebogo Nomnqa <tebzax2@gmail.com>
2022-01-18 21:55:19 +0200

    style:center align logo and convert px to rems

--- src/styles/Header.styled.js
+++ src/styles/Header.styled.js
@@ -4,17 +4,18 @@ import styled from 'styled-components';
 export const StyledHeader = styled.header`
     display: flex;
     justify-content: space-between;
-    padding: 32px 80px;
-    
+    padding: 1.64rem 5rem;
+    align-items: center;
     ul{
         display:flex;
         justify-content: space-between;
         list-style-type: none;
-        gap:25px;
+        gap:1.563rem;
     }
     a{
         text-decoration: none;
-        color: ${({ theme }) => theme.colors.gray}
+        color: ${({ theme }) => theme.colors.gray};
+        line-height: 1.219rem;
     }
     a:hover{
         color:${({ theme }) => theme.colors.accent} ;
