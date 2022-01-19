a493d96a14aca3cb8ed6038738f997d8a8611a26
Tebogo Nomnqa <tebzax2@gmail.com>
2022-01-19 21:57:31 +0200

    chore: move BrowserRouter to index.js for testing purposes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/Global';
 import Theme from './styles/Theme';
@@ -10,14 +10,12 @@ import Search from './Pages/Search';
 function App() {
   return (
     <ThemeProvider theme={Theme}>
-      <BrowserRouter>
-        <GlobalStyles />
-        <Header />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/search" element={<Search />} />
-        </Routes>
-      </BrowserRouter>
+      <GlobalStyles />
+      <Header />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
     </ThemeProvider>
   );
 }
