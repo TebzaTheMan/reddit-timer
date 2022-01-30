3ba068708b11410a6049cbbd0a5c803a9e68ffbd
Tebogo Nomnqa <tebzax2@gmail.com>
2022-01-30 15:38:55 +0200

    feat: get maximum of 500 posts

--- src/hooks/useFetchPosts.js
+++ src/hooks/useFetchPosts.js
@@ -1,10 +1,24 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
-export const fetchPosts = async (subreddit) => {
-  const res = await axios.get(`https://reddit.com/r/${subreddit}/top.json?t=year&limit=100`);
-  const data = await res.data.data.children;
-  return data;
+export const fetchPosts = async (subreddit, previousPosts = [], after = null) => {
+  let url = `https://reddit.com/r/${subreddit}/top.json?t=year&limit=100`;
+
+  if (after) {
+    url += `&after=${after}`;
+  }
+  const res = await axios.get(url);
+  const { data } = res.data;
+  const newPosts = data.children;
+  const allPosts = previousPosts.concat(newPosts);
+
+  const noMorePosts = data && data.dist < 100;
+  const limitReached = allPosts.length >= 500;
+  if (noMorePosts || limitReached) {
+    return allPosts;
+  }
+
+  return fetchPosts(subreddit, allPosts, data.after);
 };
 export const useFetchPosts = (subreddit) => {
   const [posts, setPosts] = useState(null);
